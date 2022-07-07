be5195678820c783928c7b1ba967a1f31c6f2e59
cristianmagallanes <cristian.magallanes@gmail.com>
2022-07-07 17:22:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 
 streamlit.text('Hard-Boiled free-Renge Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
