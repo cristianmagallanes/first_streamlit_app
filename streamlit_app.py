be4ce416b27984c05ba0ded12f978ffed4812f30
cristianmagallanes <cristian.magallanes@gmail.com>
2022-07-07 17:19:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
 streamlit.title ('My parents New Healty Dinner')
+
+
+streamlit.header ('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled free-Renge Egg')
